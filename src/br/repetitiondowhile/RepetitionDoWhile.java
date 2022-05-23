23a8544bdd04634310021c050e771bcc78939b4e
Marcelo Vieira <48066643+cecelo01@users.noreply.github.com>
2022-05-23 09:56:11 -0300

    up

---
+++ src/br/repetitiondowhile/RepetitionDoWhile.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package br.repetitiondowhile;
+
+/**
+ *
+ * @author Marce
+ */
+public class RepetitionDoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cc = 0 ;
+        
+        do {
+            System.out.println("Cambalhora");
+            cc++; 
+        } while (cc < 4);
+            
+        
+    }
+    
+}
